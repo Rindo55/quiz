9c114e7f0fd3fe2102c91773452e656d2d2dd2ce
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-02-03 12:07:18 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, jsonify
+import subprocess
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_question')
+def get_question():
+    # Call C program to get the next question
+    c_program_output = subprocess.check_output(['./quiz'], universal_newlines=True)
+    
+    # Parse the JSON-formatted question data
+    question_data = json.loads(c_program_output)
+    return jsonify(question_data)
+
+@app.route('/submit_answer/<int:user_answer>')
+def submit_answer(user_answer):
+    # Call C program to check the user's answer
+    c_program_output = subprocess.check_output(['./quiz', str(user_answer)], universal_newlines=True)
+    
+    # Parse the JSON-formatted result data
+    result_data = json.loads(c_program_output)
+    return jsonify(result_data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
